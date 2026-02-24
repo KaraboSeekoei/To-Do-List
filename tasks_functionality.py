67fd2c27a65381aceaf97ec3e7bd922ebf2b8599
Karabo Seekoei <karaboprincess94@gmail.com>
2026-02-24 13:07:20 +0200

    Appending tasks to a csv file

--- tasks_functionality.py
+++ tasks_functionality.py
@@ -1,5 +1,16 @@
+import csv
 tasks = [] 
 def new_page():
     task = input("Enter a new task:")
     tasks.append(task)
-    print(f"{task} has been added")
+    with open('tasks.csv', mode='a', newline='') as file:
+    # Create a CSV writer object
+        writer = csv.writer(file)
+        writer.writerow(tasks)
+
+    # Write the list as a new row
+        print(f"{task} has been added")
+        
+
+# def view_task():
+#     view = readfile()
